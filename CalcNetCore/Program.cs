a0190b74e2d83e5dcee8ae7ace08546cfb8db696
Sergey Kazulin <sergeykazulin@hotmail.com>
2020-10-08 18:40:13 +0300

    Добавьте файлы проекта.

---
+++ CalcNetCore/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading;
+
+
+namespace Training
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CALC: +, -, *, /, %, p");
+            
+            float a, b;
+            char c;
+            
+            Console.Write("Enter Number 1: ");
+            a = float.Parse(Console.ReadLine());
+            
+            Console.Write("Enter Number 2: ");
+            b = float.Parse(Console.ReadLine());
+            
+            Console.Write("Enter Action: ");
+            c = char.Parse(Console.ReadLine());
+
+            try
+            {
+                if (c == '+')
+                    Console.WriteLine("result = "+ (a + b));
+                
+                else if (c == '-')
+                    Console.WriteLine("result = "+ (a - b));
+
+                else if (c == '*')
+                    Console.WriteLine("result = "+ (a * b));
+                
+                else if (b == 0)
+                    Console.WriteLine("DivideByZeroException");
+
+                else if (c == '/')
+                    Console.WriteLine("result = "+ (a / b));
+
+                else if (c == 'm')
+                    Console.WriteLine("result = "+ (a % b));
+                
+                else if (c == ('p'))
+                    Console.WriteLine("result = " + (Math.Pow(a, b)));
+            
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("DivideByZeroException");
+                throw;
+            }
+        }
+    }
+}
